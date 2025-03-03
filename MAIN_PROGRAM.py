3c7e3eeab7f91b984258312b271cb4ea87ce96cc
MoopFace <etlielf@gmail.com>
2025-03-03 12:24:32 -0800

    changed prints again

--- MAIN_PROGRAM.py
+++ MAIN_PROGRAM.py
@@ -186,7 +186,7 @@ while cap.isOpened():
                                 set_right()
                                 move()
                                 nextTime = time.time() + (turntime) * abs(error) 
-                                print "next time is", nextTime
+                                print("next time is{0}", nextTime)
                                 moving = True
                             
 
@@ -196,7 +196,7 @@ while cap.isOpened():
                                 set_left()
                                 move()
                                 nextTime = time.time() + (turntime) * abs(error) 
-                                print "next time is", nextTime
+                                print("next time is{0}", nextTime)
                                 moving = True
                                 
 
@@ -205,7 +205,7 @@ while cap.isOpened():
                                 set_forward()
                                 move()
                                 nextTime = time.time() + movetime
-                                print "next time is", nextTime
+                                print("next time is{0}", nextTime)
                                 moving = True
                                 print("going forwards")
 
