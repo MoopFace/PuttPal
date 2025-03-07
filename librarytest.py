a1fcab8f590c8c0b4b57dbf7dde67f716b706bfd
MoopFace <etlielf@gmail.com>
2025-03-07 12:04:44 -0800

    I wouldn't know

--- librarytest.py
+++ librarytest.py
@@ -20,12 +20,13 @@ if (HIT):
     MAX_SIZE = 1000
     MAX_SIZE = 1
 
+    print("THE MODE IS ", GPIO.getmode())
+    
     GPIO.setmode(GPIO.BOARD)
     GPIO.setup(AIN1, GPIO.OUT)
     pwm = GPIO.PWM(AIN1, PWM_FREQ)
     pwm.start(0)
 
-    print("THE MODE IS ", GPIO.getmode())
 
     GPIO.output(AIN2, GPIO.LOW)
 
