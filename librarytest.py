bf943ff81c4d9c5d841c2d7a2665dd792a2f7c75
MoopFace <etlielf@gmail.com>
2025-03-07 11:54:58 -0800

    buns

---
+++ librarytest.py
@@ -0,0 +1,36 @@
+import time
+import Jetson.GPIO as GPIO
+from adafruit_motorkit import Motorkit
+
+
+#setup motorkit
+kit = MotorKit(i2c=board.I2C())
+
+
+#setup GPIO
+AIN1 = 32
+AIN2 = 33
+PWM_FREQ = 50
+
+MAX_SIZE = 1000
+MAX_SIZE = 1
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(AIN1, GPIO.OUT)
+pwm = GPIO.PWM(AIN1, PWM_FREQ)
+pwm.start(0)
+
+GPIO.output(AIN2, GPIO.LOW)
+
+
+
+
+#motorkit move
+kit.motor1.throttle=(0.5)
+time.sleep(2)
+kit.motor1.throttle=(0)
+
+#hitter swing
+pwm.ChangeDutyCycle(60)
+time.sleep(1)
+pwm.ChangeDutyCycle(0)
